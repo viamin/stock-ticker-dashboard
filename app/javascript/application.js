b230229d4bc24647dc85bb024f2bd58d6ef7b5f6
Bart Agapinan <bart@sonic.net>
2024-10-31 04:09:40 +0000

    Get tailwind loading properly

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "chartkick"
-import "Chart.bundle"
-
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import "chartkick"
+import "Chart.bundle"
---
+++ app/javascript/controllers/konami_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="konami"
+export default class extends Controller {
+  connect() {
+    this.konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]; // Up, Up, Down, Down, Left, Right, Left, Right, B, A
+    this.konamiIndex = 0;
+    this.handleKeydown = this.handleKeydown.bind(this);
+    window.addEventListener('keydown', this.handleKeydown);
+    console.log("Konami controller connected");
+  }
+
+  disconnect() {
+    window.removeEventListener('keydown', this.handleKeydown);
+  }
+
+  handleKeydown(event) {
+    console.log(event.keyCode);
+    if (event.keyCode === this.konamiCode[this.konamiIndex]) {
+      this.konamiIndex++;
+      if (this.konamiIndex === this.konamiCode.length) {
+        this.konamiIndex = 0;
+        this.redirect();
+      }
+    } else {
+      this.konamiIndex = 0;
+    }
+  }
+
+  redirect() {
+    window.location.href = "/stock/manipulate";
+  }
+}
