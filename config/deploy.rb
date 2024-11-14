dfe7c98689f81de5b6dad80c4d83edbcebf71998
Bart Agapinan <bart@sonic.net>
2024-11-14 10:16:31 +0000

    Remove sidekiq and use the built in async adapter

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,9 +38,6 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "storage", "
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-set :sidekiq_roles, :app
-set :sidekiq_processes, 1
-
 set :migration_role, :app
 
 set :assets_manifests, [ "app/assets/config/manifest.js" ]
